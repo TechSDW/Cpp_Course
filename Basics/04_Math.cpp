840efd73f071d8180c6c94d17b5957a4a3fe367c
Fabio <fbnawa@gmail.com>
2026-07-01 08:29:50 -0300

    BASICS: Adição de funções matemáticas simples frequentemente utilizadas

--- Basics/04_Math.cpp
+++ Basics/04_Math.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath> // Necessário para a seção de funções
 
 int main(){
     int x = 3;
@@ -25,6 +26,24 @@ int main(){
     // Resto da divisão (%)
     int z = x % y;
     // Resultado: 1
+
+    /* Funções Matemáticas */
+    double w;
+    double v;
+
+    // Potenciação
+    w = pow(3, 2); // Resultado: 3 ^ 2 = 9
+
+    // Radiciação
+    w = sqrt(9); // Resultado: 3
+    
+    // Modulo
+    w = abs(-4); // Resultado: 4
+
+    // Arredondamento
+    w = round(3.5); // Resultado: 4
+    w = ceil(3.1); // Resultado: 4
+    w = floor(3.9); // Resultado: 4
 
     return 0;
 }
--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ## Descrição
 Este curso foi desenvolvido para o autor consiga armazenar os conhecimentos adquiridos durante a aprendizagem, além disso, este repositório pode auxiliar outros que estejam interessados nesta linguagem.
-Pretendo tentar adicionar algum conteúdo todo o dia, contudo não tenho certeza se conseguirei
 
 ## Objetivo
 Meu objetivo é aprender enquanto eu ensino outras pessoas sobre o C++
